2d088dd48b2c419d15d67ca93eb21bb79710f0ae
jdog <thuz3rfr13ndly@gmail.com>
2022-09-19 00:35:05 -0600

    background gradient and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PageHandler from "./components/PageHandler";
 import './components/assets/styles/app.css';
-const App = () => <PageHandler />;
+const App = () => <PageHandler className='gradient' />;
 
 export default App;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,78 +1,72 @@
 import React from "react";
-import Tabs from "@mui/material/Button";
+
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import DownloadIcon from '@mui/icons-material/Download';
+import './assets/styles/app.css';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <Box
       style={{
+        display: 'flex',
+        justifyContent: 'flex-end',
         margin: 0,
-        backgroundColor: "#737b7d",
         borderRadius: "3px",
         padding: "2px 2px",
         width: "100%",
       }}
     >
-      <ul style={{ display: "flex", justifyContent: "flex-end", gap: ".5rem" }}>
-        <Button sx={{ fill: "white" }}>
-          <li style={{ listStyleType: "none" }}>
+      <ul style={{ display: "flex",  flexWrap: 'wrap', gap: ".5rem" }}>
+        <Button variant="contained" style={{ color:'white', listStyleType: "none", backgroundColor:'#2c698d' }}>
+          <li style={{ listStyleType: "none", textDecoration: "none" }}>
             <a
               style={{ textDecoration: "none" }}
               href="#home"
               onClick={() => handlePageChange("Home")}
-              className={
-                currentPage === "Home" ? "nav-link active" : "nav-link"
-              }
+              className='noActive'
             >
               Home
             </a>
           </li>
         </Button>
-        <Button variant="text">
+        <Button variant="contained" style={{ listStyleType: "none", backgroundColor:'#2c698d' }}>
           <li style={{ listStyleType: "none" }}>
             <a
-              style={{ textDecoration: "none" }}
+              style={{ textDecoration: "none", fontColor:'white' }}
               href="#about"
               onClick={() => handlePageChange("About")}
-              className={
-                currentPage === "About" ? "nav-link active" : "nav-link"
-              }
+              className='noActive'
             >
               About
             </a>
           </li>
         </Button>
-        <Tabs>
-          <li style={{ listStyleType: "none", textDecoration: "none" }}>
+        <Button variant="contained" style={{  backgroundColor:'#2c698d' }}>
+          <li style={{ listStyleType: "none", textDecoration: "none", backgroundColor:'#2c698d' }}>
             <a
               style={{ listStyleType: "none", textDecoration: "none" }}
               href="#projects"
               onClick={() => handlePageChange("Project")}
-              className={
-                currentPage === "Projects" ? "nav-link active" : "nav-link"
-              }
+              className='noActive'
             >
               Projects
             </a>
           </li>
-        </Tabs>
-        <Tabs>
+        </Button>
+        <Button variant="contained" style={{  backgroundColor:'#2c698d' }}>
           <li style={{ listStyleType: "none", textDecoration: "none" }}>
             <a
               style={{ listStyleType: "none", textDecoration: "none" }}
               href="#contact"
               onClick={() => handlePageChange("Contact")}
-              className={
-                currentPage === "Contact" ? "nav-link active" : "nav-link"
-              }
+              className='noActive'
             >
               Contact
             </a>
           </li>
-        </Tabs>
-        <Button variant= 'contained' style={{ listStyleType: "none", textDecoration: "none" , marginRight:'10px'}} endIcon={<DownloadIcon />}  href={require("../components/assets/docs/Justin Ortiz Resume.pdf")}
+        </Button >
+        <Button variant= 'contained' style={{ listStyleType: "none", textDecoration: "none" , marginRight:'10px', backgroundColor:'#2c698d'}} endIcon={<DownloadIcon />}  href={require("../components/assets/docs/Justin Ortiz Resume.pdf")}
               download="JustinOResume">
           <li style={{ listStyleType: "none", textDecoration: "none" }}>
           
