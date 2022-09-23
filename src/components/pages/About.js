ebbde7f556dbe86c9437b03e6a5e76d6052f5305
jdog <thuz3rfr13ndly@gmail.com>
2022-09-23 17:33:40 -0600

    about page styling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,16 +6,19 @@ import Paper from "@mui/material/Paper";
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1 style={{ padding:'2em', textDecoration:'underline'}}>About Me</h1>
       <section className="aboutSection">
         <img className="imgCapture" src={Capture} alt="ProfilePhoto" />
 
         <Paper className="aboutTextContainer" elevation={3}>
           <p>
-            I love building projects and apps that help solve problems and make
-            a change towards greatness in the world. Impacting the world with
-            each new idea made to make a positive change is what the world needs
-            more than ever.
+
+
+           Hello! My name is Justin. <br></br><br></br>
+           
+           My passion for programming comes paired with my professional background in facilities management paired with a deep interest in tech and video games. Since typing my first hello world app at the age of 15 I was hooked. I love building and creating mobile-friendly web apps in combination with team-collaboration, problem solving, resourcefulness, and analytical skills.
+
+
           </p>
         </Paper>
       </section>
