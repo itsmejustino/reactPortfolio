d509b6ef1c8338aa6afcdf65eadce83475f7412b
jdog <thuz3rfr13ndly@gmail.com>
2022-10-08 18:39:05 -0600

    project cards

---
+++ src/components/pages/cardFourComponent.js
@@ -0,0 +1,97 @@
+import React from "react";
+import "../assets/styles/app.css";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import PreviewIcon from "@mui/icons-material/Preview";
+import SourceIcon from "@mui/icons-material/Source";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+
+
+export default function ProjectFour({ props }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div>
+      <section className="projectContainer">
+       
+        <div className="cardsContainer">
+          <Card sx={{ maxWidth: 345, marginTop: 5 }}>
+            <CardHeader title="Pantry-Pal: A Recipe Generator" />
+            <CardMedia
+              component="img"
+              height="194"
+              image={require("../assets/pictures/readMeGen.gif")}
+              alt="Pantry-Pal"
+            />
+            <CardContent>
+              <Typography variant="body" color="text.secondary">
+               Make-a-Readme is a README.md generator for developers to help with creating a readme file for your projects. This app is a desktop application that requires installation of Node.js and a an integrated development environment of choice. Answering a list of prompts and invoking the program will create a detailed readme file. 
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <Typography color="text.secondary">Demo</Typography>
+              <IconButton aria-label="open live website">
+                <PreviewIcon
+                  onClick={(event) =>
+                    window.open("https://drive.google.com/file/d/1TMLQKNRXldldiwccZ4G7xDx3sHhjA7cG/view")
+                  }
+                />
+              </IconButton>
+              <Typography color="text.secondary">Source Code</Typography>
+              <IconButton aria-label="source code">
+                <SourceIcon
+                  onClick={(event) =>
+                    window.open("https://github.com/itsmejustino/Make-a-README")
+                  }
+                />
+              </IconButton>
+
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Technologies and Use:</Typography>
+                <Typography paragraph>
+                 This app was created with Javascript and Node.js. API calls were made with Insomnia.<br></br>
+                 This app has created many of my own readmes and includes the option
+
+            
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/components/pages/cardOneComponent.js
@@ -0,0 +1,172 @@
+import React from "react";
+import "../assets/styles/app.css";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import PreviewIcon from "@mui/icons-material/Preview";
+import SourceIcon from "@mui/icons-material/Source";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+
+
+export default function Projects({ props }) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <div>
+
+      <section className="projectContainer">
+  
+        <div className="cardsContainer">
+          <Card sx={{ maxWidth: 345, marginTop: 5 }}>
+            <CardHeader title="Pantry-Pal: A Recipe Generator" />
+            <CardMedia
+              component="img"
+              height="194"
+              image={require("../assets/pictures/PantryPal.png")}
+              alt="Pantry-Pal"
+            />
+            <CardContent>
+              <Typography variant="body" color="text.secondary">
+                Have you ever looked in the pantry and thought "I have nothing
+                good to make for dinner". Pantry-Pal seeks to help its user make
+                that tough decision of what to make for dinner with the random
+                food in the pantry. Users can search a database of recipes with
+                a list of ingredients, keywords, and/or phrases.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <Typography color="text.secondary">Live Site</Typography>
+              <IconButton aria-label="open live website">
+                <PreviewIcon
+                  onClick={(event) =>
+                    window.open("https://pantry-pal1.herokuapp.com/")
+                  }
+                />
+              </IconButton>
+              <Typography color="text.secondary">Source Code</Typography>
+              <IconButton aria-label="source code">
+                <SourceIcon
+                  onClick={(event) =>
+                    window.open("https://github.com/itsmejustino/Pantry-Pal")
+                  }
+                />
+              </IconButton>
+
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Technologies and Use:</Typography>
+                <Typography paragraph>
+                  This App was created with JavaScript, HandleBars.js, Node.js,
+                  Bootstrap and CSS. This app is hosted on Heroku as a live
+                  website. <br></br>
+                  <br></br>
+                  Note: If you would like to try out without creating an account
+                  please use this login.<br></br>
+                  <br></br>
+                  Email: 'bill@bill.bill'<br></br>
+                  Password: 'test1234'
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+
+        {/* <div>
+          <Card sx={{ maxWidth: 345, marginTop: 5 }}>
+            <CardHeader title="Pantry-Pal: A Recipe Generator" />
+            <CardMedia
+              component="img"
+              height="194"
+              image={require("../assets/pictures/PantryPal.png")}
+              alt="Pantry-Pal"
+            />
+            <CardContent>
+              <Typography variant="body3" color="text.secondary">
+                Have you ever looked in the pantry and thought "I have nothing
+                good to make for dinner". Pantry-Pal seeks to help its user make
+                that tough decision of what to make for dinner with the random
+                food in the pantry. Users can search a database of recipes with
+                a list of ingredients, keywords, and/or phrases.
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <Typography color="text.secondary">Live Site</Typography>
+              <IconButton aria-label="open live website">
+                <PreviewIcon
+                  onClick={(event) =>
+                    window.open("https://pantry-pal1.herokuapp.com/")
+                  }
+                />
+              </IconButton>
+              <Typography color="text.secondary">Source Code</Typography>
+              <IconButton aria-label="source code">
+                <SourceIcon
+                  onClick={(event) =>
+                    window.open("https://github.com/itsmejustino/Pantry-Pal")
+                  }
+                />
+              </IconButton>
+
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Technologies and Use:</Typography>
+                <Typography paragraph>
+                  This App was created with JavaScript, HandleBars.js, Node.js,
+                  Bootstrap and CSS. This app is hosted on Heroku as a live
+                  website. <br></br>
+                  <br></br>
+                  Note: If you would like to try out without creating an account
+                  please use this login.<br></br>
+                  <br></br>
+                  Email: 'bill@bill.bill'<br></br>
+                  Password: 'test1234'
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+          </div> */}
+          
+        </div>
+      </section>
+    </div>
+  );
+}
